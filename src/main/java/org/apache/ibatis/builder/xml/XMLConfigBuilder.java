350a1369b855a8b338a40e01182bebfb023e0dd3
wuchenliang1 <wuchenliang@imdada.cn>
2019-12-30 00:49:51 +0800

    提交测试代码

--- src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLConfigBuilder.java
@@ -95,6 +95,7 @@ public class XMLConfigBuilder extends BaseBuilder {
       throw new BuilderException("Each XMLConfigBuilder can only be used once.");
     }
     parsed = true;
+    //解析mybatis核心主文件
     parseConfiguration(parser.evalNode("/configuration"));
     return configuration;
   }
